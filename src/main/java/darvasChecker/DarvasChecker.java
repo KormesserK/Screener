80d50c5c58463f324a226b2b178627b4037759d6
KormesserK <35266891+KormesserK@users.noreply.github.com>
2018-08-03 22:45:18 +0200

    Mistakes
    Somehow nothing is darvas

--- src/main/java/darvasChecker/DarvasChecker.java
+++ src/main/java/darvasChecker/DarvasChecker.java
@@ -11,10 +11,12 @@ import model.Stock;
 import model.StockDay;
 
 import java.io.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class DarvasChecker {
@@ -28,6 +30,7 @@ public class DarvasChecker {
         List<Stock> darwasList = new ArrayList<>();
 
         String dirPath = "src/CSVs";
+        String stockname= "";
 
         File dir = new File(dirPath);
         File[] directoryListing = dir.listFiles();
@@ -38,13 +41,29 @@ public class DarvasChecker {
                     String line;
 
                     while((line = br.readLine())!=null){
-                        String[] splitLine = line.split(",");
-                        StockDay sd = new StockDay();
-                        sd.setStock(child.getName());
-                        sd.setDay();
+                        try {
+                            String[] splitLine = line.split(",");
+                            StockDay sd = new StockDay();
+                            sd.setStock(child.getName());
+                            stockname = child.getName();
+                            String[] dateSplit = splitLine[0].split("-");
+                            LocalDate ld = LocalDate.of(Integer.parseInt(dateSplit[0]), Integer.parseInt(dateSplit[1]), Integer.parseInt(dateSplit[2]));
+                            sd.setDay(ld);
+                            sd.setOpen(Double.parseDouble(splitLine[1]));
+                            sd.setHigh(Double.parseDouble(splitLine[2]));
+                            sd.setLow(Double.parseDouble(splitLine[3]));
+                            sd.setClose(Double.parseDouble(splitLine[4]));
+                            sd.setVolume(Long.parseLong(splitLine[6]));
+                            priceList.add(sd);
+                        }catch(Exception ex){
+                            //ex.printStackTrace();
+                        }
                     }
 
-
+                    if(isDarvas(priceList)){
+                        Stock darvas = new Stock(stockname);
+                        System.out.println(darvas.getSymbol());
+                    }
 
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
@@ -59,7 +78,7 @@ public class DarvasChecker {
 
     public static Boolean isDarvas(List<StockDay> data){
         StockDay low = getLowWeeks(data);
-        if(low.getClose()*1.70>data.get(0).getClose()){
+        if(low.getClose()*1.70>data.get(data.size()-1).getClose()){
             return false;
         }
         if(!hasHigh(data)){
@@ -71,7 +90,7 @@ public class DarvasChecker {
     }
 
     public static StockDay getLowWeeks(List<StockDay> data){
-        LocalDateTime day = LocalDateTime.now().minus(52, ChronoUnit.WEEKS);
+        LocalDate day = LocalDate.now().minus(52, ChronoUnit.WEEKS);
         StockDay low = new StockDay();
 
         for (StockDay sd: data ) {
@@ -81,7 +100,7 @@ public class DarvasChecker {
         }
 
         for (StockDay sd : data){
-            if(low.getClose()<sd.getLow()){
+            if(low.getClose()>sd.getLow() && sd.getDay().isAfter(day)){
                 low = sd;
             }
         }
